f2035c0e5a60e5ff28a083e4524cd1b46cb2e915
Imbalancement <138077219+Imbalancement@users.noreply.github.com>
2024-09-23 20:38:27 -0400

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Custom output directory for build
+  
   distDir: 'build',
   
-  // For static export
-  //output: 'export',
 
-  // Custom image loader setup
-  /*images: {
-    loader: 'custom',
-    loaderFile: './src/loader.js',  // Correct relative path to loader.js
-  },*/
+
 };
 
 export default nextConfig;
