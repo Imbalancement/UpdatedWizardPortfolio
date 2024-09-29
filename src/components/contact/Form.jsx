bf7aaf77284071785d1dca5e3c410022f5043dbe
Imbalancement <138077219+Imbalancement@users.noreply.github.com>
2024-09-29 11:43:34 -0400

    Add files via upload

--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -28,49 +28,53 @@ export default function Form() {
     formState: { errors },
   } = useForm();
 
-  const sendEmail = (params) => {
-    const toastId = toast.loading("Sending your message, please wait...");
+  
+  
+  
+  
+  // const sendEmail = (params) => {
+  //   const toastId = toast.loading("Sending your message, please wait...");
 
-    toast.info(
-      "Form submissions are demo-only here. Please checkout the final code repo to enable it. If you want to connect you can reach out to me via codebucks27@gmail.com.",
-      {
-        id: toastId,
-      }
-    );
+  //   toast.info(
+  //     "Form submissions are demo-only here. Please checkout the final code repo to enable it. If you want to connect you can reach out to me via codebucks27@gmail.com.",
+  //     {
+  //       id: toastId,
+  //     }
+  //   );
 
-    // comment out the above toast.info and uncomment the below code to enable emailjs
+    //comment out the above toast.info and uncomment the below code to enable emailjs
 
-    // emailjs
-    //   .send(
-    //     process.env.NEXT_PUBLIC_SERVICE_ID,
-    //     process.env.NEXT_PUBLIC_TEMPLATE_ID,
-    //     params,
-    //     {
-    //       publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY,
-    //       limitRate: {
-    //         throttle: 5000, // you can not send more then 1 email per 5 seconds
-    //       },
-    //     }
-    //   )
-    //   .then(
-    //     () => {
-    //       toast.success(
-    //         "I have received your message, I will get back to you soon!",
-    //         {
-    //           id: toastId,
-    //         }
-    //       );
-    //     },
-    //     (error) => {
-    //       // console.log("FAILED...", error.text);
-    //       toast.error(
-    //         "There was an error sending your message, please try again later!",
-    //         {
-    //           id: toastId,
-    //         }
-    //       );
-    //     }
-    //   );
+    emailjs
+      .send(
+        process.env.NEXT_PUBLIC_SERVICE_ID,
+        process.env.NEXT_PUBLIC_TEMPLATE_ID,
+        params,
+        {
+          publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY,
+          limitRate: {
+            throttle: 5000, // you can not send more then 1 email per 5 seconds
+          },
+        }
+      )
+      .then(
+        () => {
+          toast.success(
+            "I have received your message, I will get back to you soon!",
+            {
+              id: toastId,
+            }
+          );
+        },
+        (error) => {
+          // console.log("FAILED...", error.text);
+          toast.error(
+            "There was an error sending your message, please try again later!",
+            {
+              id: toastId,
+            }
+          );
+        }
+      );
   };
 
   const onSubmit = (data) => {
@@ -157,4 +161,4 @@ export default function Form() {
       </motion.form>
     </>
   );
-}
+// }
